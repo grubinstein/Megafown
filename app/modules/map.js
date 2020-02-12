516f98f52cda1ff30dbcf59f29d781950e2e5047
Gabriel Rubinstein <gabrielrubinstein1@googlemail.com>
2020-02-12 22:22:09 +0000

    Refactored marker and listitem creation and click listeners

--- app/modules/map.js
+++ app/modules/map.js
@@ -1,110 +1,118 @@
 import axios from 'axios';
 import { $, $$ } from './bling';
+const markers = [];
+const listItems = [];
+const infoWindow = new google.maps.InfoWindow();
 
-const mapOptions = {
-    center: { lat: 43.2, lng: -79.8 },
-    zoom: 15
-}
-// could get location from navigator.geolocation.getCurrentPosition
-
-function loadPlaces(map, lat = 43.2, lng = -79.8) {
+const fetchCasts = (map, lat = 43.2, lng = -79.8) => {
     axios.post('/api/nearby-casts', {
         lat,
         lng
     })
-        .then(res => {
-            const casts = res.data;
-            if(!casts.length) {
-                alert('no casts found!');
-                return;
-            }
+    .then(res => loadCasts(res.data, map, lat, lng));
+}
 
-            const bounds = new google.maps.LatLngBounds();
-            const infoWindow = new google.maps.InfoWindow();
-            
-            addMyLocationToMap(new google.maps.LatLng(lat, lng), map);
-            bounds.extend({lat, lng});
-            
-            const listItems = casts.map(createListItem);
-            
-            const markers = casts.map((cast,i) => {
-                const [castLng, castLat] = cast.location.coordinates;
-                const position = { lat: castLat, lng: castLng };
-                bounds.extend(position);
-                const marker = new google.maps.Marker( { 
-                    map, 
-                    position,
-                    icon: 'http://maps.gstatic.com/mapfiles/markers2/marker.png'
-                });
-                marker.cast = cast;
-                marker.addListener('click', function() {
-                    markers.forEach(marker => {
-                        marker.setIcon('http://maps.gstatic.com/mapfiles/markers2/marker.png');
-                    });
-                    this.setIcon('//maps.gstatic.com/mapfiles/markers2/icon_green.png');
-
-                    const html = `
-                        <div class="popup">
-                            <p><strong>${this.cast.name}</strong></p>
-                            <p>Created ${moment(this.cast.created).fromNow()}</p>
-                        </div>
-                    `;
-                    infoWindow.setContent(html);
-                    infoWindow.open(map, this);
-                    
-                    $$(".list-group-item").forEach(el => el.classList.remove("active"));
-                    listItems[i].classList.add("active");
-                });
-                marker.addListener('click', function() {
-            })
-                return marker;
-            });
-
-            listItems.forEach((listItem,i) => {
-                listItem.on('click', function() {
-                    markers.forEach(marker => {
-                        marker.setIcon('http://maps.gstatic.com/mapfiles/markers2/marker.png');
-                    });
-                    markers[i].setIcon('//maps.gstatic.com/mapfiles/markers2/icon_green.png');
-                    $$(".list-group-item").forEach(el => el.classList.remove("active"));
-                    this.classList.add("active");
-                })
-            })
-            
-            const center = bounds.getCenter();
-            map.setCenter(center);
-            map.fitBounds(bounds, 15);
-        })
-        .catch(console.error);
+const loadCasts = ( casts, map, lat, lng ) => {
+    if(!casts.length) {
+        alert('no casts found!');
+        return;
+    }    
     
-}
+    const bounds = new google.maps.LatLngBounds();
+    
+    addMyLocationToMap(new google.maps.LatLng(lat, lng), map);
+    bounds.extend({lat, lng});
+    
+    casts.forEach((cast,i) => {
+        markers[i] = createMarker(cast, i, map, bounds);
+        listItems[i] = createListItem(cast, i);
+    });    
 
-function addMyLocationToMap(position, map) {
+    const center = bounds.getCenter();
+    map.setCenter(center);
+    map.fitBounds(bounds, 15);
+
+};    
+
+const addMyLocationToMap = (position, map) => {
     var myLocation = new google.maps.Marker({
-                clickable: false,
-                position,
-                icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
-                                                    new google.maps.Size(22,22),
-                                                    new google.maps.Point(0,18),
-                                                    new google.maps.Point(11,11)),
-                shadow: null,
-                zIndex: 999,
-                map
-            });
+        clickable: false,
+        position,
+        icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+        new google.maps.Size(22,22),
+        new google.maps.Point(0,18),
+        new google.maps.Point(11,11)),
+        shadow: null,
+        zIndex: 999,
+        map
+    });
 }
 
-function createListItem(cast) {
+const createMarker = (cast, i, map, bounds) => {
+    const [castLng, castLat] = cast.location.coordinates;
+    const position = { lat: castLat, lng: castLng };
+
+    bounds.extend(position);
+    
+    const marker = new google.maps.Marker( { 
+        map, 
+        position,
+        icon: 'http://maps.gstatic.com/mapfiles/markers2/marker.png'
+    });    
+    marker.cast = cast;
+    marker.castIndex = i;
+
+    marker.addListener('click', function() {
+        selectCast(this);        
+    });    
+    
+    return marker;
+}    
+
+const createListItem = (cast, i) => {
     const listItem = document.createElement("li");
     listItem.className = "list-group-item";
     listItem.innerHTML = `
                             <p><strong>${cast.name}</strong></p>
                             <p>Started ${moment(cast.created).fromNow()}</p>
                         `;
+    listItem.cast = cast;
+    listItem.castIndex = i;
+
+    listItem.on('click', function() {
+        selectCast(this);
+    })
+
     $(".list-group").appendChild(listItem);
     return listItem;
 }
 
+const selectCast = (clicked) => {
+    markers.forEach(marker => {
+        marker.setIcon('http://maps.gstatic.com/mapfiles/markers2/marker.png');
+    });    
+
+    const marker = markers[clicked.castIndex];
+    marker.setIcon('//maps.gstatic.com/mapfiles/markers2/icon_green.png');
+    
+    const html = `
+                    <div class="popup">
+                    <p><strong>${clicked.cast.name}</strong></p>
+                    <p>Created ${moment(clicked.cast.created).fromNow()}</p>
+                    </div>
+                `;
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+    
+    $$(".list-group-item").forEach(el => el.classList.remove("active"));
+    listItems[clicked.castIndex].classList.add("active");
+}
+
 function makeMap(mapDiv) {
+    const mapOptions = {
+        center: { lat: 43.2, lng: -79.8 },
+        zoom: 15
+    }
     const map = new google.maps.Map(mapDiv, mapOptions);
 
     $(".location").on("locationChanged", (event) => {
@@ -112,7 +120,7 @@ function makeMap(mapDiv) {
             lat: event.detail.coordinates[0],
             lng: event.detail.coordinates[1]
         };
-        loadPlaces(map, event.detail.coordinates[0], event.detail.coordinates[1]);
+        fetchCasts(map, event.detail.coordinates[0], event.detail.coordinates[1]);
         map.setCenter(coordinates);
     });
     /*loadPlaces(map);

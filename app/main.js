c1aa31668b9705b64ae6e9d5123a79ca27b7a8ad
Gabriel Rubinstein <gabrielrubinstein1@googlemail.com>
2020-02-13 10:01:29 +0000

    Moved call to makeMap to main to improve readability

--- app/main.js
+++ app/main.js
@@ -3,9 +3,10 @@ import './main.scss';
 import './modules/speak';
 import './modules/autocomplete'; 
 import makeMap from './modules/map';
+import { $ } from './modules/bling'
 import { removeFlash } from './modules/flashes'
 
-
+$('#map') && makeMap($('#map'));
 /*
 const callButton = $("#call-btn");
 callButton && callButton.on("click", makeCall);
--- app/modules/map.js
+++ app/modules/map.js
@@ -125,6 +125,4 @@ function makeMap(mapDiv) {
     });
 };
 
-$('#map') && makeMap($('#map'));
-
 export default makeMap

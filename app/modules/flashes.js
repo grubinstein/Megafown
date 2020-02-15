f50e00be922972e985abbc1c6d2078c086ed95ff
Gabriel Rubinstein <gabrielrubinstein1@googlemail.com>
2020-02-15 10:18:01 +0000

    catchErrors uses function declaration to allow access to arguments

--- app/modules/flashes.js
+++ app/modules/flashes.js
@@ -2,7 +2,7 @@
 require('regenerator-runtime/runtime');
 import { $ } from './bling';
 
-const catchErrors = (fn, options) => async () => {    
+const catchErrors = (fn, options = {}) => async function() {
     const { msg, onFail } = options;
     removeErrorFlash();
     return await fn(...arguments).catch(err => {
@@ -16,7 +16,7 @@ const newErrorFlash = (err, msg) => {
     removeErrorFlash();
     const flashDiv = $(".client-flash");
     const text = $(".client-flash > .alert > p")
-    text.innerText = msg || err.message || err;
+    text.innerText = msg || (err && err.message) || err;
     flashDiv.style.display = "block";
     $(".remove-client-flash").on("click", removeErrorFlash);
 }

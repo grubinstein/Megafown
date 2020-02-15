6d7e312d835c72ce9c070207b3f9d043c321f63d
Gabriel Rubinstein <gabrielrubinstein1@googlemail.com>
2020-02-15 11:24:00 +0000

    Removed commented peer funcitonality from main.js

--- app/main.js
+++ app/main.js
@@ -7,33 +7,3 @@ import { $, $$ } from './modules/bling'
 import { removeFlash } from './modules/flashes'
 
 $('#map') && makeMap($('#map'));
-/*
-const callButton = $("#call-btn");
-callButton && callButton.on("click", makeCall);
-
-const castButton = $("#cast-btn");
-castButton && castButton.on("click", prepareForCast);
-
-const makeCall = () => {  
-	const remoteId = document.getElementById("remoteID").value;
-	
-	var conn = peer.connect(remoteId);
-}
-
-peer.on('connection', (conn) => {
-	conn.on('open', () => {
-		console.log("New connection from " + conn.peer);
-		var call = peer.call(conn.peer, window.localStream)
-	})
-});
-
-peer.on('call', (call) => {
-	console.log("Received call");
-	call.answer();
-	call.on("stream", (remoteStream) => {
-		const player = document.getElementById("audio-player");
-		player.srcObject = remoteStream;
-		player.play();
-	})
-})
-*/

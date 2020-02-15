8c60d4d997aa7ca651b385d2e059534086c6f864
Gabriel Rubinstein <gabrielrubinstein1@googlemail.com>
2020-02-15 09:37:41 +0000

    Created compound index to replace multiple single indexes

--- models/Peer.js
+++ models/Peer.js
@@ -32,7 +32,6 @@ const peerSchema = new mongoose.Schema({
     }
 });
 
-peerSchema.index({ connected: 1 });
-peerSchema.index({ downstreamPeers: 1 });
+peerSchema.index({ cast: 1, tier: 1 }, { partialFilterExpression: { downstreamPeers: { $lt: 2 }}};
 
 module.exports = mongoose.model('Peer', peerSchema);
